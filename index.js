9a14121fb6a3896137b466cab146089f6e78784c
Sahana Suresh <116923402+sahanas0@users.noreply.github.com>
2026-04-13 22:29:39 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
+/* 
+This program is the logic of the main page. It first gets the longitude and latitude of the inputted zip using Nominatim
+and inputs those parameters to FEMA's official map server. FEMA returns the FLD Zone, and if it is high, the program outputs
+so.
+*/
+
 document.getElementById("continue").addEventListener("click", lookupZip);
 
 async function lookupZip() {
   const zip = document.getElementById("enterZip").value.trim();
   const result = document.getElementById("result");
 
-  // Validate ZIP
+  /* Checking if the zip is 5 characters long */
   if (zip.length !== 5 || isNaN(zip)) {
     result.textContent = "Please enter a valid 5-digit ZIP code.";
     return;
   }
 
   try {
-    // --- STEP 1: ZIP → LAT/LNG using Nominatim ---
+    /* Using the zip code, I am using Nominatim's open street map to find the latitude and longitude */
     const nominatimURL = `https://nominatim.openstreetmap.org/search?postalcode=${zip}&country=USA&format=json`;
+    let message = "";
 
     const geoResponse = await fetch(nominatimURL);
     const geoData = await geoResponse.json();
 
+    /* If no data is returned, it is not a real/valid zip code */
     if (geoData.length === 0) {
       result.textContent = "ZIP code not found.";
       return;
@@ -25,9 +33,10 @@ async function lookupZip() {
     const lat = geoData[0].lat;
     const lng = geoData[0].lon;
 
+    /* Error checking the coordinates w/ the console in browser */
     console.log("Coordinates:", lat, lng);
 
-    // --- STEP 2: FEMA Flood Zone ---
+    /* Using FEMA map server data to get the zone based on the latitude and longitude of the zip code */
     const femaURL = `https://hazards.fema.gov/arcgis/rest/services/public/NFHL/MapServer/28/query?geometry=${lng},${lat}&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&outFields=FLD_ZONE&returnGeometry=false&f=json`;
 
     const femaResponse = await fetch(femaURL);
@@ -39,12 +48,13 @@ async function lookupZip() {
     }
 
     console.log("Flood Zone:", zone);
-
-    // --- Display result nicely ---
-    let message = "";
-
-    if (zone === "AE" || zone === "A") {
+    
+    /* The program outputs the corresponding message to the flood zone returned by FEMA */
+    if (zone === "AE" || zone === "A" || zone === "AH" || zone === "AO" || zone === "AR" || zone === "A99" || zone === "V" || zone === "VE") {
       message = `High Risk of Flood ⚠️ Zone: ${zone} `;
+    } 
+    else if (zone === "D") {
+      message = `Unknown Risk of Flood at This Time 🌦️ Zone: ${zone} `;
     } else {
       message = `Low Risk of Flood ☀️ Zone: ${zone}`;
     }
